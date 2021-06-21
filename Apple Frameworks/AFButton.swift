861c359958141b70cec595ced347407fb92464dc
Frank <fsolleveld@outlook.com>
2021-06-21 19:40:42 +0200

    frameworkdetailview

---
+++ Apple Frameworks/AFButton.swift
@@ -0,0 +1,27 @@
+//
+//  AFButton.swift
+//  Apple Frameworks
+//
+//  Created by Frank Solleveld on 21/06/2021.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var title: String
+    var body: some View {
+        Text(title)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 280, height: 50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+struct AFButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AFButton(title: "Test Title")
+    }
+}
