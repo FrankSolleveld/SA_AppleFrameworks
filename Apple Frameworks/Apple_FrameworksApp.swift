f5178c854cc5ce29ee5b65c9ab98c5863c5337be
Frank <fsolleveld@outlook.com>
2021-06-21 11:43:17 +0200

    Initial Commit

---
+++ Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by Frank Solleveld on 21/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
