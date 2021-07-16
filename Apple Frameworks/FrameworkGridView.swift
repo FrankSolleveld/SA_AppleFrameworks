235ffca73e06cdc4b73e4d644faf2e21a6aeca2d
Frank <fsolleveld@outlook.com>
2021-07-16 11:24:11 +0200

    X button now works too

--- Apple Frameworks/FrameworkGridView.swift
+++ Apple Frameworks/FrameworkGridView.swift
@@ -27,7 +27,9 @@ struct FrameworkGridView: View {
             }
             .navigationTitle("🍏 Frameworks")
 			.sheet(isPresented: $viewModel.isShowingDetailView) {
-				FrameworkDetailView(framework: viewModel.selectedFramework ?? MockData.sampleFramework)
+				FrameworkDetailView(framework: viewModel.selectedFramework
+									?? MockData.sampleFramework,
+									isShowingDetailView: $viewModel.isShowingDetailView)
 			}
         }
     }
