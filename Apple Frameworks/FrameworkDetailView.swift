a909e257151e6de659a6fdd215b7b02deca6f654
Frank <fsolleveld@outlook.com>
2021-07-16 11:34:50 +0200

    added a safari view

--- Apple Frameworks/FrameworkDetailView.swift
+++ Apple Frameworks/FrameworkDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct FrameworkDetailView: View {
     var framework: Framework
 	@Binding var isShowingDetailView: Bool
+	@State private var isShowingSafariView = false
     var body: some View {
         VStack {
             HStack {
@@ -31,11 +32,14 @@ struct FrameworkDetailView: View {
                 .padding()
             Spacer()
             Button {
-                
+                isShowingSafariView = true
             } label: {
                 AFButton(title: "Learn More")
             }
         }
+		.sheet(isPresented: $isShowingSafariView) {
+			SafariView(url: URL(string: framework.urlString) ?? URL(string: "https://devfrank.org")!)
+		}
     }
 }
 
